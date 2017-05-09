1ba4be134b5778e55901f5b357d3013440e34d8a
Mike Ralphson <mike.ralphson@gmail.com>
2017-05-09 18:28:19 +0100

    buildCaches; cope with repeats/overlaps

--- buildCaches.js
+++ buildCaches.js
@@ -6,57 +6,65 @@ var fs = require('fs');
 
 var readfiles = require('node-readfiles');
 
-var radio = '';
-var tv = '';
+var radio = {service:{type:'Radio'},items:{}};
+var tv = {service:{type:'TV'},items:{}};
 var tvIndex = 1;
 var radioIndex = 10000;
 var tvProgs = 0;
 var radioProgs = 0;
-const now = new Date()/1000;
+const now = Math.floor(new Date()/1000);
 const aMonth = 30*24*60*60;
 
 var heading = '#index|type|name|pid|available|expires|episode|seriesnum|episodenum|versions|duration|desc|channel|categories|thumbnail|timeadded|guidance|web';
 
-tv += heading + '\n';
-radio += heading + '\n';
+function output(obj,filename) {
+	var s = heading + '\n';
+	for (var pid in obj.items) {
+		var item = obj.items[pid];
+		if (obj.service.type == 'TV') {
+			s += (tvIndex++)+'|tv|';
+			tvProgs++;
+		}
+		else {
+			s += (radioIndex++) + '|radio|';
+			radioProgs++;
+		}
+		s += item.brand.title + '|';
+		s += item.episode.id + '|';
+		s += item.published_time.end + '|';
+		s += (new Date(item.published_time.end)/1000)+aMonth + '|';
+		s += item.episode.title + '|';
+		s += '1|';
+		s += '1|';
+		s += 'original|';
+		s += '0|';
+		s += '|';
+		s += item.service.id + '|';
+		s += '|';
+		s += '|';
+		s += now + '|';
+		s += '|';
+		s += 'http://bbc.co.uk/programmes/'+item.episode.id + '|';
+		s += '\n';
+	}
+	fs.writeFile(filename,s,'utf8');
+}
 
 function processSchedule(filename) {
-	if (filename.indexOf('package.json')<0) {
-		var scheduleTxt = fs.readFileSync(filename,'utf8');
-		var schedule = JSON.parse(scheduleTxt);
-		for (let item of schedule.items) {
-			var s = '';
-			if (schedule.service.type == 'TV') {
-				s += (tvIndex++)+'|tv|';
-				tvProgs++;
+	var scheduleTxt = fs.readFileSync(filename,'utf8');
+	var schedule = JSON.parse(scheduleTxt);
+	for (let item of schedule.items) {
+		var target = (schedule.service.type == 'TV' ? tv : radio);
+		if (target.items[item.episode.id]) {
+			if (new Date(item.published_time.start) < new Date(target.items[item.episode.id].published_time.start)) {
+				target.items[item.episode.id].published_time.start = item.published_time.start;
 			}
-			else {
-				s += (radioIndex++) + '|radio|';
-				radioProgs++;
+			if (new Date(item.published_time.end) > new Date(target.items[item.episode.id].published_time.end)) {
+				target.items[item.episode.id].published_time.end = item.published_time.end;
 			}
-			s += item.brand.title + '|';
-			s += item.episode.id + '|';
-			s += item.published_time.end + '|';
-			s += (new Date(item.published_time.end)/1000)+aMonth + '|';
-			s += item.episode.title + '|';
-			s += '1|';
-			s += '1|';
-			s += 'original|';
-			s += '0|';
-			s += '|';
-			s += item.service.id + '|';
-			s += '|';
-			s += '|';
-			s += now + '|';
-			s += '|';
-			s += 'http://bbc.co.uk/programmes/'+item.episode.id + '|';
-			s += '\n';
-			if (schedule.service.type == 'TV') {
-				tv += s;
-			}
-			else {
-				radio += s;
-			}
+		}
+		else {
+			target.items[item.episode.id] = item;
 		}
 	}
 }
@@ -72,15 +80,15 @@ readfiles(pathspec, {readContents: false, filenameFormat: readfiles.FULL_PATH}, 
 		return 0;
 	});
 	for (var file of files) {
-		if ((file.indexOf('.json')>0) && (file.indexOf('node_modules')<0)) {
-			//console.log(file);
+		if ((file.indexOf('.json')>0) && (file.indexOf('package.json')<0) && (file.indexOf('node_modules')<0)) {
 			processSchedule(file);
 		}
 	}
+
+	output(radio,'./radio.cache');
+	output(tv,'./tv.cache');
 	console.log('Radio programmes '+radioProgs);
 	console.log('TV programmes '+tvProgs);
-	fs.writeFileSync('./radio.cache',radio,'utf8');
-	fs.writeFileSync('./tv.cache',tv,'utf8');
 })
 .catch(err => {
 	console.log(util.inspect(err));
